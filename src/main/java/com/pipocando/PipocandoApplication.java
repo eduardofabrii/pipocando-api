c64e96754ca25dcaf3ea66e06f7aa31ca1ff0320
Eduardo Fabri <eduardohfabri@gmail.com>
2025-04-02 22:27:21 -0300

    Inicializando repositório

---
+++ src/main/java/com/pipocando/PipocandoApplication.java
@@ -0,0 +1,13 @@
+package com.pipocando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipocandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipocandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pipocando/PipocandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipocando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipocandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
