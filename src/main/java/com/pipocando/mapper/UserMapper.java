7884e43c3e85a05d8ae6b437892839734570a4a0
Eduardo Fabri <eduardohfabri@gmail.com>
2025-04-03 10:54:53 -0300

    feat(User): add UserMapper para mapear User para UserGetResponse

---
+++ src/main/java/com/pipocando/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.pipocando.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+import com.pipocando.domain.user.User;
+import com.pipocando.dto.response.UserGetResponse;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface UserMapper {
+
+    List<UserGetResponse> toUserGetResponseList(List<User> users); 
+}
