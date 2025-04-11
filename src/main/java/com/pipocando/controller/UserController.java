8a239ec8146f7002b53906cb9a9076aaec22f7ac
Jcliz <joaopedrocliz@gmail.com>
2025-04-11 17:24:50 -0300

    feat(registration): cadastro de usuário

--- src/main/java/com/pipocando/controller/UserController.java
+++ src/main/java/com/pipocando/controller/UserController.java
@@ -4,16 +4,20 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pipocando.dto.registration.RegistrationDTO;
 import com.pipocando.dto.response.UserGetResponse;
 import com.pipocando.service.user.UserService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("v1/user")
+@RequestMapping("register")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -22,5 +26,11 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<UserGetResponse>> listAll() {
         return ResponseEntity.ok(userService.findAllUsers());
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> registerUser(@RequestBody @Valid RegistrationDTO registrationDTO) {
+        userService.registerUser(registrationDTO);
+        return ResponseEntity.ok().build();
     }
 }
