98cd180c068bfee7e7bf791e1f865447dadfa15f
Eduardo Fabri <eduardo.fabri@wises.com.br>
2025-04-16 19:48:21 -0300

    feat(User): add UserPostRequest e UserPutRequest

---
+++ src/main/java/com/pipocando/dto/update/UserPutRequest.java
@@ -0,0 +1,19 @@
+package com.pipocando.dto.update;
+
+import com.pipocando.domain.user.UserRole;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserPutRequest(
+    @NotBlank(message = "Nome é obrigatório")
+    String name,
+    
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    String email,
+    
+    UserRole role,
+    
+    Boolean active
+) {}
